ec5b3c408203656dcf1f9b1291040ea2f2b8c24d
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2021-12-31 16:30:16 +0300

    Update Q4.py

--- contest/Q4.py
+++ contest/Q4.py
@@ -13,3 +13,33 @@
                 continue
             print(m,end=' ')
         print(" ")
+'''
+b,
+'''
+      n=4
+
+    for i in range(1,n+1):
+        for j in range(n-i):
+            print(' ' ,end=' ')
+        for k in range(1,i+1):
+           print(k,end=' ')
+        for m in range(i-1,0,-1):
+            if i==1:
+                continue
+            print(m,end=' ')
+        print(" ")
+    for i in range(1,n):
+        for j in range(1,i+1):
+            print(" ",end=" ")
+        for k in range(1,n-i+1):
+            print(k,end=" ")
+        for m in range(n-i-1,0,-1):
+            print(m,end=' ')
+        print(' ')
+
+'''
+c,
+'''
+
+
+  
