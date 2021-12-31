684c6d627437261c3055284b12398eb89b9a4005
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2021-12-31 17:02:15 +0300

    Update Q4.py

--- contest/Q4.py
+++ contest/Q4.py
@@ -40,6 +40,14 @@ b,
 '''
 c,
 '''
-
+  n=5
+    for i in range(n):
+       if i%2==0:
+           for j in range(n):
+                print('##  ', end=' ')
+       else:
+           for j in range(n-1):
+                print('  ##',end=' ')
+       print()
 
   
