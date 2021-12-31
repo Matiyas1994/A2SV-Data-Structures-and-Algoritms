410f0c1ffd6df87d81e854bfc04a0dc0ba282a2a
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2021-12-31 14:44:30 +0300

    Create Q3.py

---
+++ contest/Q3.py
@@ -0,0 +1,9 @@
+  '''
+  print a pattern, time-O(n) space-O(1)
+  '''
+  n=4
+  for i in range(n):
+    print('#' * (n - 1), end=' ')
+  print('')
+  for i in range(n):
+    print('#' * (n - 1), end=' ')
