aa8866779bdef35ab73f65ae1abb81c763a95ddc
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-01-03 18:32:29 +0300

    Update and rename progress_shit/Array Partition.py to progress_sheet/Array Partition.py

--- progress_sheet/Array Partition.py
+++ progress_sheet/Array Partition.py
@@ -1,7 +1,10 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
+         '''
+        time-O(nlogn)  space-O(1)
+        '''
         nums.sort()
         sum=0
         for i in range(0,len(nums),+2):
-            sum+=min(nums[i],nums[i+1])
+            sum+=nums[i]
         return sum
