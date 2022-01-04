927d6f803237a1ce78232b49ce499a7cdb55b0ff
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-01-04 11:19:27 +0300

    Update Third Maximum Number.py

--- progress_sheet/Third Maximum Number.py
+++ progress_sheet/Third Maximum Number.py
@@ -1,5 +1,8 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
+        '''
+        time-O(nlogn)//sorting is the bottle neck // space-O(n)
+        '''
         s=sorted(list(set(nums)))
         if len(s)>=3:
             return s[-3]
