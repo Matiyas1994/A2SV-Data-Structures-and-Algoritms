4f022d2a3a692cc3e5d42eb70daba2bd6831ee40
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-01-11 17:12:01 +0300

    Update Eliminate Maximum Number of Monsters.py

--- Daily_problems/Eliminate Maximum Number of Monsters.py
+++ Daily_problems/Eliminate Maximum Number of Monsters.py
@@ -1,23 +1,14 @@
 class Solution:
     def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
-        time=[round(dist[i]/speed[i],2) for i in range(len(dist))]
+        time=[dist[i]/speed[i] for i in range(len(dist))]
         time.sort()
-        time.remove(time[0])
-        count=1
-        for i in range(len(time)):
-            time[i]-=1
-        if len(time)!=0 and time[0] >0:
-            islive=True
-        else:
-            islive=False
-        while islive:
-            if len(time)!=0 and time[0] >0:
-                time.remove(time[0])
-                for i in range(len(time)):
-                    time[i]-=1 
-                count +=1
-            else: 
-                islive=False
+        count=0
+        t=0
+        
+        for i in time:
+            if i<= t:
+                break
+            else:
+                t+=1
+                count+=1
         return count
-            
-       
