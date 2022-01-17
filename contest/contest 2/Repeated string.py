ea6321dfa431d3284255b83e57097c87508ab221
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-01-17 13:39:01 +0300

    Update Repeated string.py

--- contest/contest 2/Repeated string.py
+++ contest/contest 2/Repeated string.py
@@ -1,1 +1,41 @@
-code
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'repeatedString' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. LONG_INTEGER n
+#
+
+def repeatedString(s, n):
+    # Write your code here
+    from collections import Counter
+    sdict=Counter(s)
+    res=n%len(s)
+    addition=0
+    for i in range(res):
+        if s[i]=='a':
+            addition +=1
+    guranted=int(n/len(s))
+    return (sdict['a'] * guranted + addition) 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input().strip())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
