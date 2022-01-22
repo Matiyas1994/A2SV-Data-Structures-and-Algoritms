317ea44c3188a4dbc079352210178007fe2f4da8
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-01-22 20:14:53 +0300

    Create Number of Recent Calls.py

---
+++ stack/Number of Recent Calls.py
@@ -0,0 +1,19 @@
+class RecentCounter:
+
+    def __init__(self):
+        self.requests=[]
+        self.count=0
+
+    def ping(self, t: int) -> int:
+        self.requests.append(t)
+        while t-3000 > self.requests[0] :
+            self.requests.pop(0)
+        return len(self.requests)
+            
+        
+        
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
