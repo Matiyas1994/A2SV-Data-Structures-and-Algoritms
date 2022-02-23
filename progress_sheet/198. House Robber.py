ba334e93947d7c3ac2109250908f868760327ab2
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-02-24 02:20:38 +0300

    Update 198. House Robber.py

--- progress_sheet/198. House Robber.py
+++ progress_sheet/198. House Robber.py
@@ -11,9 +11,9 @@ class Solution:
         return max(evenSum,oddSum)
         '''
         
-        dp = [0,nums[0]]
+        Maxrobery = [0,nums[0]]
         
         for i in range(1,len(nums)):
-            dp.append (max(dp[i],dp[i-1] + nums[i]))
+            Maxrobery.append (max(Maxrobery[i],Maxrobery[i-1] + nums[i]))
             
-        return dp[len(nums)]
+        return Maxrobery[len(nums)]
