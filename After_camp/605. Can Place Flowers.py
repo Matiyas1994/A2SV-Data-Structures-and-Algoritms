c27f0ef3ba723e8d3e833917df80df84d611c3a9
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-04-07 16:52:50 +0300

    Create 605. Can Place Flowers.py

---
+++ After_camp/605. Can Place Flowers.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        
+        flowerbed = [0] + flowerbed +[0]
+        for i in range(1,len(flowerbed)-1):
+            if flowerbed[i]==0 and flowerbed[i-1] == 0 and flowerbed[i+1]==0:
+                flowerbed[i] = 1
+                n -=1
+            if n <= 0:
+                return True
+               
